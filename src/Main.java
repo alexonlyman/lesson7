71ef32158e2a059dbe0e1a7b85206159bbea673e
Алекс <chu@mail.ru>
2022-10-04 14:10:59 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         //задание 1
+        for ( int i=0; i<10; i++){
+            System.out.println("Отсчет " + i);
+        }
+        for ( int a=10; a>0; a--) {
+            System.out.println("Отсчет "+ a );
+        }
 
+        for ( int b =0; b<17; b=b+2) {
+            System.out.println("четные " + b);
+        }
+        for ( int c=10; c>-11; c--) {
+            System.out.println("От большего " + c);
+        }
 
 
 
